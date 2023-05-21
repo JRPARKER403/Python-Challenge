63f300c4a133fe275dba551c42e1a3c7fdd06b46
JESSICA PARKER <122657255+JRPARKER403@users.noreply.github.com>
2023-05-20 23:34:05 -0500

    updates

--- PyPoll/main.py/election_data.py
+++ PyPoll/main.py/election_data.py
@@ -1,39 +1,35 @@
 import os
 import csv
 
-#election_data.csv ='"C:\Users\JMARS\OneDrive\Desktop\Bootcamp\HOMEWORK\python-challenge\PyPoll\Resources\election_data.csv"'
-election_data = os.path.join("/election_data.csv")
+#election_data.csv ="C:\Users\JMARS\OneDrive\Desktop\Bootcamp\HOMEWORK\python-challenge\PyPoll\Resources\election_data.csv"
 
-# Open and read csv
-with open(election_data) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-
-    # Read the header row first (skip this part if there is no header)
-    csv_header = next(csv_reader)
-    print(f"Header: {csv_header}")
+filename = 'election_data.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
 
     #variables
-    candidates = []
-    num_votes = 0
-    vote_counts = []
-    percent_votes = []
-    total_votes = []
-    max_votes = vote_counts[0]
-    max_index = 0
-    candidate = []
-
+candidates = []
+num_votes = 0
+vote_counts = []
+percent_votes = []
+total_votes = []
+max_votes = vote_counts[0]
+max_index = 0
+candidate = []
+row = []
 
     #total number of votes
-    num_votes = num_votes + 1
+num_votes = num_votes + 1
 
     #voted for candidate
-    candiate = row[2]
+candiate = row [2]
 
     #if other votes are present for a candidate
-    if candidate in candidates:
+if candidate in candidates:
         candidate_index = candidates.index(candidate)
         vote_counts[candidate_index] = vote_counts[candidate_index] + 1
-    else:
+else:
         candidates.append(candidates)
         vote_counts.append(1)
    
